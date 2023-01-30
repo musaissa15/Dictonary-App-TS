2a53a16ce90cfda8b170e5d2c569052bc850bbaf
Musa Imad Issa <=>
2023-01-30 16:46:12 +0000

    import input componet to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
 import './App.css';
+import Input from './Input';
 
 function App() {
   return (
     <div className="App">
       <h1> Dictionary</h1>
+      <Input/>
     </div>
   );
 }
