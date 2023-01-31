ee0c4966ac5fc93b6bf10b8c4d1916fd5696ac49
Musa Imad Issa <=>
2023-01-31 23:54:27 +0000

    import useState from react

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Input from './Input';
 
 function App() {
+  const [word, setWord] = useState<string>('')
   return (
-    <div className="App">
-      <h1> Dictionary</h1>
-      <Input/>
-    </div>
-  );
+		<div className="App">
+			<h1> Dictionary</h1>
+			<Input word={word} setWord={setWord} />
+		</div>
+	);
 }
 //!!https://api.dictionaryapi.dev/api/v2/entries/en/<word>
 export default App;
