d2adce6a045b8b661359e6b08c7a2ee0d77be819
Musa Imad Issa <=>
2023-02-01 00:05:58 +0000

    pass input value to state func

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,19 +1,20 @@
 import React, { FormEventHandler, ChangeEventHandler } from "react";
 
 type Props = {
-	word: string};
+	word: string;
+	setWord: (value: string) => void;
+};
 
 const Input = (props: Props) => {
 	const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
 		event.preventDefault();
 	};
 
-	const handleChange = (
-		event: React.ChangeEvent<HTMLInputElement> 
-	): void => {
-		console.log(event.target.value);
-		console.log(event);
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+		props.setWord(event.target.value);
+		
 	};
+console.log(props.word);
 
 	return (
 		<div>
