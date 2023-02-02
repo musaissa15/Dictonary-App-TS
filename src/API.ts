4f041a78d1a489754ba515bbea6333f3b056c892
Musa Imad Issa <=>
2023-02-02 13:58:00 +0000

    make Api call

--- src/API.ts
+++ src/API.ts
@@ -4,6 +4,10 @@ const mainUrl = axios.create({
      baseURL: 'https://api.dictionaryapi.dev/api/v2/entries/en'
 })
 
-
+const getWordDate = async (searchedWord: string) => {
+    const { data } = await mainUrl.get(`${searchedWord}`);
+   
+    
+}
  
 
