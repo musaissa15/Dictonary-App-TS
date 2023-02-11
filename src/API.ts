20927390f958e456b36729733da19b04f08af57c
Musa Imad Issa <musaissa004@gmail.com>
2023-02-11 18:27:29 +0000

    clear commented code

--- src/API.ts
+++ src/API.ts
@@ -1,8 +1,5 @@
 import axios from "axios"
 
-// const mainUrl = axios.create({
-//      baseURL: 'https://api.dictionaryapi.dev/api/v2/entries/en'
-// })
 
  export const getWordData = async (searchedWord: string) => {
     const { data } = await axios.get(
