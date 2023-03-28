0a93310ee4125e8c45439d6f07816a0f6b3f5786
Musa Imad Issa <musaissa004@gmail.com>
2023-03-29 00:59:51 +0100

    add button component

--- src/Input.tsx
+++ src/Input.tsx
@@ -69,7 +69,7 @@ const Input=({searchWord, setSearchWord}: Props) => {
 					 className="object-center 
     "
 				/>
-				<button type='submit'> Search</button>
+				<button type='submit' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"> Search</button>
 			</form>
 			<ul >
 				{wordInfo.map(w => {
