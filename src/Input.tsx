c24f8cec47954b3e01c9fe666b8ae537cd9176fa
Musa Imad Issa <musaissa004@gmail.com>
2023-04-10 02:30:53 +0100

    style header and form with tailwind css

--- src/Input.tsx
+++ src/Input.tsx
@@ -36,9 +36,9 @@ type Dictionary = {
 	};
 	sourceUrls: string[];
 }[];
-const Input=({searchWord, setSearchWord}: Props) => {
+const Input = ({searchWord, setSearchWord}: Props) => {
 	const [wordInfo, setWordInfo] = useState<Dictionary>([]);
-	const handleSubmit = (event:  any): void => {
+	const handleSubmit = (event: any): void => {
 		event.preventDefault();
 	};
 
@@ -62,28 +62,32 @@ const Input=({searchWord, setSearchWord}: Props) => {
 	return (
 		<div>
 			<form onSubmit={handleSubmit}>
-			<div className="pt-2 relative mx-auto text-gray-600">
-				<input
-					type='text'
-					onChange ={handleChange}
-						value={searchWord}
-						placeholder="Search"
-					 className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none object-center 
-    "
-				/>
-					<button type='submit' className="absolute right-0 top-0 mt-5 mr-4"> <svg className="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
-            viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xmlSpace="preserve"
-            width="512px" height="512px">
-            <path
-              d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-          </svg></button>
+			<div className="flex justify-start border-b-4 title">
+					<h1 className=' inline-block md:inline text-4xl '> Dictionary</h1>
 					</div>
+				<div className='  mx-auto text-pink-600 form'>
+					<input
+						type='text'
+						onChange={handleChange}
+						value={searchWord}
+						placeholder='Enter a Word'
+						className='border-2 border-red-400 rounded-lg input'
+					/>
+					<button
+						type='submit'
+						className='button rounded-lg border-2 border-red-200'>
+						Search
+					</button>
+					</div>
+					
 			</form>
-			<ul >
+			<br />
+			<br />
+			<br />
+			<ul>
 				{wordInfo.map(w => {
 					return (
-						<li >
+						<li>
 							<h1>{w.word}</h1>
 							<h5>{w.meanings[0].partOfSpeech} </h5>
 							<h5> {w.phonetic} </h5>
@@ -100,11 +104,11 @@ const Input=({searchWord, setSearchWord}: Props) => {
 								</h3>
 							) : w.meanings[0].synonyms.length > 1 ? (
 								<h3>
-									Synonyms: {w.meanings[0].synonyms.join(", ")}
+									Synonyms:{" "}
+									{w.meanings[0].synonyms.join(", ")}
 								</h3>
-							) : w.meanings[0].synonyms.length === 0 ? (
-								null
-							) : null}
+							) : w.meanings[0].synonyms.length ===
+							  0 ? null : null}
 
 							{w.license ? (
 								<a href={w.license?.url}>{w.license?.name}</a>
@@ -122,7 +126,7 @@ const Input=({searchWord, setSearchWord}: Props) => {
 										<ReactAudioPlayer
 											src={phonetic.audio}
 											controls
-											className=" items-center"
+											className=' items-center'
 										/>
 									) : null}
 								</div>
